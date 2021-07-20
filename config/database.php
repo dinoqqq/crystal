251fbdc83bc783e12b080980b1e38fab9e248e9e
Daan Kuijsten <daan@kuijstenwebcompany.nl>
2021-07-20 17:25:48 +0200

    remove trailing comma

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'database_name' => '',
-    'username' => '',
-    'password' => '',
+    'database_name' => 'test_bbutler_testing',
+    'username' => 'bb',
+    'password' => 'bb',
 
     'database_type' => 'mysql',
     'server' => '127.0.0.1',
