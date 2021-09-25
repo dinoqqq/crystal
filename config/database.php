91364be665f2913ed0007c43f3b36857cd0655c2
Daan <daan@kuijstenwebcompany.nl>
2021-09-25 12:57:09 +0200

    use service container

--- config/database.php
+++ config/database.php
@@ -3,9 +3,9 @@ return [
     'database_name' => 'test',
     'username' => 'root',
     'password' => 'root',
+    'server' => 'myMysqlHost',
 
     'database_type' => 'mysql',
-    'server' => '127.0.0.1',
 
     'charset' => 'utf8',
     'collation' => 'utf8_general_ci',
